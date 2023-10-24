b75e467ebb28ca3728a9ec85f24653a94596928a
RamazanE56 <hantek5656@gmail.com>
2023-10-25 00:14:44 +0300

    24/10/2023/ismail

--- src/test/java/tests/US_018_Ismail/TC_01.java
+++ src/test/java/tests/US_018_Ismail/TC_01.java
@@ -1,12 +1,12 @@
-package tests.US_018;
+package tests.US_018_Ismail;
 
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import pages.LoginPage;
 import pages.SmartcardlinkPage;
 import utilities.ConfigReader;
 import utilities.Driver;
-import utilities.ReusableMethods;
 
 public class TC_01 {
 
@@ -20,13 +20,16 @@ public class TC_01 {
         smartcardlinkPage.signinButtonElementi.click();
         //email ve Password değerlerini girip Login butonuna tıklayınız
         Actions actions = new Actions(Driver.getDriver());
+        LoginPage loginPage = new LoginPage();
 
-        actions.click(smartcardlinkPage.emailKutusuElementi);
+
+
+        actions.click(loginPage.emailKutusuElementi);
         actions.sendKeys(ConfigReader.getProperty("semail"));
         actions.sendKeys(Keys.TAB);
         actions.sendKeys(Keys.TAB);
         actions.sendKeys(ConfigReader.getProperty("spassword")).perform();
-        smartcardlinkPage.loginElementi.click();
+        loginPage.loginElementi.click();
         //
 
 
