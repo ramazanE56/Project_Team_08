4c124f2902d23c51bc28ef332a535c974c8865fe
EMRE BAYRAM <140724234+Emre23-QA@users.noreply.github.com>
2023-10-30 23:17:29 +0300

    hhh

--- src/test/java/pages/ADashboardPage.java
+++ src/test/java/pages/ADashboardPage.java
@@ -11,105 +11,122 @@ public class ADashboardPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(xpath = "//*[@id=\"dropdownMenuButton1\"]")
-    public WebElement profildropdownMenuButton1;
-    @FindBy(xpath = "(//a[@class=\"dropdown-item text-gray-900\"])[1]")
-    public WebElement accountSettingsLinki;
-    @FindBy (xpath = "//*[text()=\"Profile Details\"]")
-    public WebElement profileDetailsLinki;
-    ////https://qa.smartcardlink.com/profile/edit altındaki firstName kutusu
-    @FindBy(xpath = "//*[@id=\"editProfileFirstName\"]")
-    public WebElement firstNameKutusu;
-
-    //https://qa.smartcardlink.com/profile/edit altınadaki save buttonu
-    @FindBy(xpath = "//*[@class=\"btn btn-primary me-2\"]")
-    public  WebElement saveButtonProfile;
 
 
 
 
-    @FindBy (xpath = "//a[@class='nav-link p-0 active']")
-    public WebElement visitorDashboard;
-
-    //Admin Dasboard sayfasında üstteki Dashboard yazısının locate'i
-    @FindBy(xpath = "//a[@class='nav-link p-0 active']")
-    public WebElement dashboardYaziElementi;
-
-
-    //Admin Dasboard sayfasında Total Active Users sayısının locate'i
-    @FindBy(xpath = "(//h2[@class='fs-1-xxl fw-bolder text-white'])[1]")
-    public WebElement totalActiveUsersElementi;
-
-    //Admin Dasboard sayfasında Total Active VCards sayısının locate'i
-    @FindBy(xpath = "(//h2[@class='fs-1-xxl fw-bolder text-white'])[2]")
-    public WebElement totalActiveVcardsElementi;
-
-    //Admin Dasboard sayfasında Total DeActive Users sayısının locate'i
-    @FindBy(xpath = "(//h2[@class='fs-1-xxl fw-bolder text-white'])[3]")
-    public WebElement totalDeActiveUsersElementi;
-
-    //Admin Dasboard sayfasında Total DeActive VCards sayısının locate'i
-    @FindBy(xpath = "(//h2[@class='fs-1-xxl fw-bolder text-white'])[4]")
-    public WebElement totalDeActiveVcardsElementi;
-
-    //Admin Dasboard sayfasında Plan oranları tablosunun locate'i
-    @FindBy(xpath = "//canvas[@id='dashboardPlanPieChart']")
-    public WebElement plansbyUsersElementi;
-
-    //Admin Dasboard sayfasında kullanıcı gelir grafiğinin locate'i
-    @FindBy(xpath = "//canvas[@id='dashboardIncomeChart']")
-    public WebElement graficElementi;
-
-    //Admin Dasboard sayfasında günlük kullanıcı kayıtlarının locate'i
-    @FindBy(xpath = "//button[@class='nav-link active p-0']")
-    public WebElement gunlukElementi;
-
-    //Admin Dasboard sayfasında haftalık kullanıcı kayıtlarının locate'i
-    @FindBy(xpath = "//button[@id='vweekData']")
-    public WebElement haftalikElementi;
-
-    //Admin Dasboard sayfasında haftalık kullanıcı kayıtlarının locate'i
-    @FindBy(xpath = "//button[@id='monthData']")
-    public WebElement aylikElementi;
-
-    //Admin Dasboard sayfasında Vcard Templates sekmesinin locate'i
-    @FindBy(xpath = "(//span[@class='aside-menu-title'])[5]")
-    public WebElement vcardTemplatesElementi;
-
-
-    //Admin Dasboard sayfasında 3 çizginin yanındaki yazının locate'i
-    @FindBy(xpath = "//a[@class='nav-link p-0 active']")
-    public WebElement sekmeBasligiYaziElementi;
 
 
 
-    //Abone olunan kullanıcı planları elementi
-    @FindBy(xpath = "//span[text()='Subscribed User Plans']")
-    public WebElement subscribedUserPlansElementi;
 
-    //Abonelik planlarını görüldü test elementi
-    @FindBy(xpath = "//h5[text()='Subscribed Plan Details']")
-    public WebElement SubscribedUserPlansTestElementi;
 
-    //Abonelik planlarını görüntüleme elementi
-    @FindBy(xpath = "(//a[@data-id='330'])[1]")
-    public WebElement SubscribedUserPlansViewElementi;
 
-    //User menu elementi
-    @FindBy(xpath = "(//*[@data-icon='users'])[1]")
-    public WebElement userMenuElementi;
 
-    //Users menusu Impersonate(kimliğe bürün) elementi
-    @FindBy(xpath = "//div[@class='d-flex justify-content-center']")
-    public WebElement impersonateElementi;
 
-    //Kullanıcıdan admin hesabına geçiş ikonu elementi
-    @FindBy(xpath = "//span[@class='text-primary']")
-    public WebElement adminHesabaGecisElementi;
 
-    //admin kullanıcı profil ismi elementi
-    @FindBy(xpath ="//button[@id='dropdownMenuButton1']")
-    public WebElement kullaniciProfilIsimElementi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
